e032b9cdf4f7489bb993995b1a8512415e1f45fa
Qiwen Lyu <qiwenlyu@wifi-131-179-45-74.host.ucla.edu>
2019-05-17 14:29:41 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# CS205 Final Project - Spring 2019
+
+## The Task:
+In this project we will try and build a predicitve model to predict being positive to cancer or malignancy.
+- Using the features available at [CDC NHANES](https://wwwn.cdc.gov/nchs/nhanes/continuousnhanes/default.aspx?BeginYear=2015), investigate what features you would like to use in your model. 
+- Under the tab "Data, Documentation, Codebooks, SAS Code" choose one of the categories: "Demographics Data", "Dietary Data", "Examination Data", "Laboratory Data", or "Questionnaire Data". 
+- For each of these categories there is a list of sub-categories available. Clicking the "Doc File" link will provide the information about features in that sub-category and feature names.
+- Using these features, construct a predictive model that will be able to predict having cancer or malignancy.
+
+**Example finding feature**: Our target of focus (cancer or malignancy) is found under Questionnaire Data -> Medical Conditions -> MCQ220 - Ever told you had cancer or malignancy. Under this question the variable name **MCQ220** is what we will use as an identifier in our code.
+
+**Tip**: If you are looking for a feature, another option is to google "NHANES \<feature name\>". This is effective if you have a feature in mind but don't want to go over all possible categories to find it.
+
+**Important**: When selecting features to process, remember to input the correct category where the feature can be found, so that the processing code can find it.
+  
+### What you are scored on
+The grade for this task will be seperated into 3 categories
+- **Feature selection, and evaluation**. You will be required to explain why certain features were incorporated. empiric evaluation (e.g: mutual information) or a logical explanation for a group of features (e.g: age, geneder are included as demographics, as certain types of cancer affect certain demographics with higher probability. As can be seen [here](https://gis.cdc.gov/Cancer/USCS/DataViz.html)).
+- **Preprocessing and feature engineering**. Writing your own preprocessing code, explain why you chose a specific imputation technique, or changed the features in a certain way. You can support you argument with previous research done on the topic or with your own experimentation.
+- **Predictive modeling**. Build an ML model to predict having cancer. Construct a model to fit the data, explain why the chosen model was selected, describe experiments done with the model (e.g: hyper parameter tuning).
+
+### How you are scored
+The grade will be given based on the quality, supported arguments, and clarity of evaluation of each of the 3 parts.
+Each part is equal 33.3333...% of your final project grade. Bonus points for originality and "out of the box" thinking for approaching each of these 3 parts.
+
+
+## File list:
+- **nhanes.py:** implementation of the data preprocessing logic as well as definition an example dataset.
+- **Demo_Dataset.ipynb:** Jupyter notebook file to demonstrate the basic usage of sample dataset.
+
+## How to use:
+1) Download [raw data files](https://drive.google.com/file/d/1hFp7O747408D8t5442f0Sjit7wXKXI1z/view?usp=sharing) and decompress them.
+2) Install Python 3 and the following packages: joblib, numpy, pandas, matplotlib, scipy, sklearn, jupyter, pytorch.
+3) Use Demo_Dataset.ipynb to see an example on how to use the predefined task.
+4) Expand nhanes.py to define new tasks by following the implementation logic of the provided sample.
+
+
+##### For a detailed explanation of the methods used here for the cost-sensetive health dataset, please refer to: ["Nutrition and Health Data for Cost-Sensitive Learning"](https://arxiv.org/abs/1902.07102)
---
+++ nhanes.py
@@ -0,0 +1,247 @@
+import pdb
+import glob
+import copy
+import os
+import pickle
+import joblib
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats
+import sklearn.feature_selection
+
+class FeatureColumn:
+    def __init__(self, category, field, preprocessor, args=None, cost=None):
+        self.category = category
+        self.field = field
+        self.preprocessor = preprocessor
+        self.args = args
+        self.data = None
+        self.cost = cost
+
+class NHANES:
+    def __init__(self, db_path=None, columns=None):
+        self.db_path = db_path
+        self.columns = columns # Depricated
+        self.dataset = None # Depricated
+        self.column_data = None
+        self.column_info = None
+        self.df_features = None
+        self.df_targets = None
+        self.costs = None
+
+    def process(self):
+        df = None
+        cache = {}
+        # collect relevant data
+        df = []
+        for fe_col in self.columns:
+            sheet = fe_col.category
+            field = fe_col.field
+            data_files = glob.glob(self.db_path+sheet+'/*.XPT')
+            df_col = []
+            for dfile in data_files:
+                print(80*' ', end='\r')
+                print('\rProcessing: ' + dfile.split('/')[-1], end='')
+                # read the file
+                if dfile in cache:
+                    df_tmp = cache[dfile]
+                else:
+                    df_tmp = pd.read_sas(dfile)
+                    cache[dfile] = df_tmp
+                # skip of there is no SEQN
+                if 'SEQN' not in df_tmp.columns:
+                    continue
+                #df_tmp.set_index('SEQN')
+                # skip if there is nothing interseting there
+                sel_cols = set(df_tmp.columns).intersection([field])
+                if not sel_cols:
+                    continue
+                else:
+                    df_tmp = df_tmp[['SEQN'] + list(sel_cols)]
+                    df_tmp.set_index('SEQN', inplace=True)
+                    df_col.append(df_tmp)
+
+            try:
+                df_col = pd.concat(df_col)
+            except:
+                #raise Error('Failed to process' + field)
+                raise Exception('Failed to process' + field)
+            df.append(df_col)
+        df = pd.concat(df, axis=1)
+        #df = pd.merge(df, df_sel, how='outer')
+        # do preprocessing steps
+        df_proc = []#[df['SEQN']]
+        for fe_col in self.columns:
+            field = fe_col.field
+            fe_col.data = df[field].copy()
+            # do preprocessing
+            if fe_col.preprocessor is not None:
+                prepr_col = fe_col.preprocessor(df[field], fe_col.args)
+            else:
+                prepr_col = df[field]
+            # handle the 1 to many
+            if (len(prepr_col.shape) > 1):
+                fe_col.cost = [fe_col.cost] * prepr_col.shape[1]
+            else:
+                fe_col.cost = [fe_col.cost]
+            df_proc.append(prepr_col)
+        self.dataset = pd.concat(df_proc, axis=1)
+        return self.dataset
+    
+    
+# Preprocessing functions
+def preproc_onehot(df_col, args=None):
+    return pd.get_dummies(df_col, prefix=df_col.name, prefix_sep='#')
+
+def preproc_real(df_col, args=None):
+    if args is None:
+        args={'cutoff':np.inf}
+    # other answers as nan
+    df_col[df_col > args['cutoff']] = np.nan
+    # nan replaced by mean
+    df_col[pd.isna(df_col)] = df_col.mean()
+    # statistical normalization
+    df_col = (df_col-df_col.mean()) / df_col.std()
+    return df_col
+
+def preproc_impute(df_col, args=None):
+    # nan replaced by mean
+    df_col[pd.isna(df_col)] = df_col.mean()
+    return df_col
+
+def preproc_cut(df_col, bins):
+    # limit values to the bins range
+    df_col = df_col[df_col >= bins[0]]
+    df_col = df_col[df_col <= bins[-1]]
+    return pd.cut(df_col.iloc[:,0], bins, labels=False)
+
+def preproc_dropna(df_col, args=None):
+    df_col.dropna(axis=0, how='any', inplace=True)
+    return df_col
+
+#### Add your own preprocessing functions ####
+
+# Dataset loader
+class Dataset():
+    """ 
+    Dataset manager class
+    """
+    def  __init__(self, data_path=None):
+        """
+        Class intitializer.
+        """
+        # set database path
+        if data_path == None:
+            self.data_path = './run_data/'
+        else:
+            self.data_path = data_path
+        # feature and target vecotrs
+        self.features = None
+        self.targets = None
+        self.costs = None
+        
+    def load_arthritis(self, opts=None):
+        columns = [
+            # TARGET: systolic BP average
+            FeatureColumn('Questionnaire', 'MCQ160A', 
+                                    None, None),
+            # Gender
+            FeatureColumn('Demographics', 'RIAGENDR', 
+                                 preproc_real, None),
+            # Age at time of screening
+            FeatureColumn('Demographics', 'RIDAGEYR', 
+                                 preproc_real, None),
+            FeatureColumn('Demographics', 'RIDRETH3', 
+                                 preproc_onehot, None),
+            # Race/ethnicity
+            FeatureColumn('Demographics', 'RIDRETH1', 
+                                 preproc_onehot, None),
+            # Annual household income
+            FeatureColumn('Demographics', 'INDHHINC', 
+                                 preproc_real, {'cutoff':11}),
+            # Education level
+            FeatureColumn('Demographics', 'DMDEDUC2', 
+                                 preproc_real, {'cutoff':5}),
+            # BMI
+            FeatureColumn('Examination', 'BMXBMI', 
+                                 preproc_real, None),
+            # Waist
+            FeatureColumn('Examination', 'BMXWAIST', 
+                                 preproc_real, None),
+            # Height
+            FeatureColumn('Examination', 'BMXHT', 
+                                 preproc_real, None),
+            # Upper Leg Length
+            FeatureColumn('Examination', 'BMXLEG', 
+                                 preproc_real, None),
+            # Weight
+            FeatureColumn('Examination', 'BMXWT', 
+                                 preproc_real, None),
+            # Total Cholesterol
+            FeatureColumn('Laboratory', 'LBXTC', 
+                                 preproc_real, None),
+            # Alcohol consumption
+            FeatureColumn('Questionnaire', 'ALQ101', 
+                                 preproc_real, {'cutoff':2}),
+            FeatureColumn('Questionnaire', 'ALQ120Q', 
+                                 preproc_real, {'cutoff':365}),
+            # Vigorous work activity
+            FeatureColumn('Questionnaire', 'PAQ605', 
+                                 preproc_real, {'cutoff':2}),
+            FeatureColumn('Questionnaire', 'PAQ620', 
+                                 preproc_real, {'cutoff':2}),
+            FeatureColumn('Questionnaire', 'PAQ180', 
+                                 preproc_real, {'cutoff':4}),
+            FeatureColumn('Questionnaire', 'PAD615', 
+                                 preproc_real, {'cutoff':780}),
+            # Doctor told overweight (risk factor)
+            FeatureColumn('Questionnaire', 'MCQ160J', 
+                                 preproc_onehot, {'cutoff':2}),
+            # Sleep
+            FeatureColumn('Questionnaire', 'SLD010H', 
+                                 preproc_real, {'cutoff':12}),
+            # Smoking
+            FeatureColumn('Questionnaire', 'SMQ020', 
+                                 preproc_onehot, None),
+            FeatureColumn('Questionnaire', 'SMD030', 
+                                 preproc_real, {'cutoff':72}),
+            # Blood relatives with arthritis
+            FeatureColumn('Questionnaire', 'MCQ250D',
+                                 preproc_onehot, {'cutoff':2}),
+            # joint pain/aching/stiffness in past year
+            FeatureColumn('Questionnaire', 'MPQ010',
+                                 preproc_onehot, {'cutoff':2}),
+            # symptoms began only because of injury
+            FeatureColumn('Questionnaire', 'MPQ030',
+                                 preproc_onehot, {'cutoff':2}),
+            # how long experiencing pain
+            FeatureColumn('Questionnaire', 'MPQ110',
+                                 preproc_real, {'cutoff':4}),
+        ]
+        nhanes_dataset = NHANES(self.data_path, columns)
+        df = nhanes_dataset.process()
+        fe_cols = df.drop(['MCQ160A'], axis=1)
+        features = fe_cols.values
+        target = df['MCQ160A'].values
+        # remove nan labeled samples
+        inds_valid = ~ np.isnan(target)
+        features = features[inds_valid]
+        target = target[inds_valid]
+
+        # Put each person in the corresponding bin
+        targets = np.zeros(target.shape[0])
+        targets[target == 1] = 0 # yes arthritis
+        targets[target == 2] = 1 # no arthritis
+
+       # random permutation
+        perm = np.random.permutation(targets.shape[0])
+        self.features = features[perm]
+        self.targets = targets[perm]
+        self.costs = [c.cost for c in columns[1:]]
+        self.costs = np.array(
+            [item for sublist in self.costs for item in sublist])
+        
+        
+    #### Add your own dataset loader ####
